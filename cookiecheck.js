e71b598f300bac4a1848b076462906b8dba63e97
Mathieu Lutfy <mathieu@bidon.ca>
2014-02-11 18:49:36 -0500

    JS redirect: only set a variable (it was double-reloading).

--- cookiecheck.js
+++ cookiecheck.js
@@ -4,8 +4,7 @@ cj(function($) {
       $('#crm-container').prepend('<div class="messages crm-error"><div class="icon red-icon alert-icon"></div>' + ts('This page requires cookies to be enabled in your browser settings. Please check this setting and enable cookies (if they are not enabled), then refresh this page. If this error persists, please contact us for assistance.') + '</div>');
     }
     else {
-      window.location += '#cookieredirect';
-      window.location.reload(false);
+      window.location += '&cookieredirect=1';
     }
   }
 });
